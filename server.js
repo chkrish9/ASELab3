bb66817b1a29f5809ecf068600082aa7aaaa7acc
Murali <murali.24.4.91@gmail.com>
2018-11-01 10:12:30 -0500

    added code

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const toDoRoutes = require('./routes/todo.js');
 const socketIO = require('socket.io');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
